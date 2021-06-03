2aa68b0da547933f28def995116be9d49ac9662e
coleb <cole.ballard97@gmail.com>
2021-06-03 11:57:07 -0600

    cleared previous routes and added basic web get route

--- routes.js
+++ routes.js
@@ -1,33 +1,24 @@
-const notes = require('./notes.json');
 const path = require('path');
-const fs = require('fs');
+const mysql = require('mysql');
+require('dotenv').config();
 
-const setJSON = data => {
-  fs.writeFileSync('notes.json', JSON.stringify(data));
-};
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect();
 
-module.exports = (app) => {
+module.exports = app => {
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 
-  app.get('/api/notes', (req, res) => res.json(notes));
+  app.get('/api/web', (req, res) => {
+    connection.query('SELECT * FROM bubbles', (err, rows, fields) => {
+      if (err) throw err;
 
-  app.get('/api/notes/:title', (req, res) => {
-    const note = notes.find(note => note.title === req.params.title);
-
-    if (!note) return res.json(false);
-    else return res.json(note);
+      res.json(rows);
+    });
   });
-
-  app.post('/api/notes', (req, res) => {
-    notes.push(req.body);
-    setJSON(notes);
-  });
-
-  app.delete('/api/notes/:title', (req, res) => {
-    notes.splice(notes.findIndex(note => note.title === req.params.title), 1);
-    setJSON(notes);
-  });
+  
 };
+
+connection.end();
